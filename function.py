880b1fdb155cde72b37a4f90091114dfcbb4a539
mvkkumar <venkatakiran.matturi@gmail.com>
2017-07-26 11:19:16 -0700

    Update function.py
    replace the string '.' with '123456789'

--- function.py
+++ function.py
@@ -6,7 +6,19 @@ def cross(a,b):
   
 
 def grid_lines(grid):
-  return dict(zip(cross(rows,cols),grid))
+      """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
+
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
+    """
+    Lgrid = list(grid)
+    Lgrid = [a if a != '.' else '123456789' for a in Lgrid]
+    
+  return dict(zip(cross(rows,cols),Lgrid))
   
 def __init__ == __main__:
   rows = 'ABCDEFGHI'
